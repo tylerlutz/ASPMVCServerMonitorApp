a33d6d1e2e315cb6e4a83fa2aa807245d77dc88e
Tyler Lutz <tlutz.computers@gmail.com>
2016-03-02 21:49:05 -0500

    Added hub to project that gets the server info

---
+++ ASPMonitorWebApp/Hubs/ServerInfo.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNet.SignalR;
+
+namespace ASPMonitorWebApp.Hubs
+{
+    public class ServerInfo : Hub
+    {
+        public void SendServerInfo(string machineName, double processor, int memoryUsage, int totalMemory)
+        {
+            this.Clients.All.serverInfoMessage(machineName, processor, memoryUsage, totalMemory);
+        }
+    }
+}
