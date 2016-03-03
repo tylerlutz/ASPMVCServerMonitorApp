7ea99b1c263cf5598b011675434c7b3dcfcb746e
Tyler Lutz <tlutz.computers@gmail.com>
2016-03-02 21:40:39 -0500

    Added SignalIR to project and created the Startup Class for SignalIR

---
+++ ASPMonitorWebApp/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartup(typeof(ASPMonitorWebApp.Startup))]
+namespace ASPMonitorWebApp
+{
+    public class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            app.MapSignalR();
+        }
+    }
+}
