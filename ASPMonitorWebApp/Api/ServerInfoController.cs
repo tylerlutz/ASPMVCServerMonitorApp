3d9b07e0cebf0f6d3aceb20751e1a651b4c53e2f
Tyler Lutz <tlutz.computers@gmail.com>
2016-03-02 22:47:25 -0500

    Added API to the project.

---
+++ ASPMonitorWebApp/Api/ServerInfoController.cs
@@ -0,0 +1,17 @@
+﻿using System.Web.Http;
+using Microsoft.AspNet.SignalR;
+using ASPMonitorWebApp.Hubs;
+using ASPMonitorWebApp.Models;
+
+
+namespace ASPMonitorWebApp.Api
+{
+    public class ServerInfoController : ApiController
+    {
+        public void Post(ServerInfoPostData serverInfo)
+        {
+            var context = GlobalHost.ConnectionManager.GetHubContext("ServerInfo");
+            context.Clients.All.cpuInfoMessage(serverInfo.MachineName, serverInfo.Processor, serverInfo.MemUsage, serverInfo.TotalMemory);
+        }
+    }
+}
---
+++ ASPMonitorWebApp/Models/ServerInfoPostData.cs
@@ -0,0 +1,10 @@
+﻿namespace ASPMonitorWebApp.Models
+{
+    public class ServerInfoPostData
+    {
+        public string MachineName { get; set; }
+        public double Processor { get; set; }
+        public ulong MemUsage { get; set; }
+        public ulong TotalMemory { get; set; }
+    }
+}
